d496bc465c3ac5ae40f3573a90622b1c269e1406
AtreusV <arturohenriquezespitia@gmail.com>
2023-04-27 07:53:14 -0500

    primer commit

---
+++ src/main/java/com/example/libro_984/Libro984Application.java
@@ -0,0 +1,13 @@
+package com.example.libro_984;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Libro984Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Libro984Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libro_984/Libro984ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libro_984;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Libro984ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
